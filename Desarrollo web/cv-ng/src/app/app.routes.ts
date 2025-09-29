2e14bf29450ad8df2ab666ba2d01dd49059a62f5
Panito115 <jpmadriz@ufm.edu>
2025-09-29 15:55:58 -0600

    update
    se agregaron comentarios y se cambio la ruta de skills por habilidades

--- Desarrollo web/cv-ng/src/app/app.routes.ts
+++ Desarrollo web/cv-ng/src/app/app.routes.ts
@@ -3,11 +3,11 @@ import { CvPage } from './pages/cv-page/cv-page';
 import { ExperiencePage } from './pages/experience-page/experience-page';
 import { SkillsPage } from './pages/skills-page/skills-page';
 import { EducationPage } from './pages/education-page/education-page';
-
+// rutas
 export const routes: Routes = [
-  { path: '', component: CvPage },       // raíz -> tu CV
-  { path: 'experience', component: ExperiencePage }, // página de experiencia ampliada
-  { path: 'skills', component: SkillsPage },
-  { path: 'education', component: EducationPage },
-  { path: '**', redirectTo: '' }         // cualquier otra ruta -> CV
+  { path: '', component: CvPage },       
+  { path: 'experience', component: ExperiencePage },  // Ruta de experiencias
+  { path: 'habilidades', component: SkillsPage }, // Ruta de habilidades
+  { path: 'education', component: EducationPage }, // Ruta de eduación
+  { path: '**', redirectTo: '' }         
 ];
